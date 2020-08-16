44d165f25b55fbd189ef3d8931e1ac8a4371b361
JOHNS JOSEPH <35776080+DEVELOPER-IN-PROGRESS@users.noreply.github.com>
2020-08-16 13:33:10 +0530

    fixed palindrome check conditions
    previously in your while loop you only checked the beginning and end characters in this way   "madam"  or "alaska" were palindromes as per your code 
    i have updated to check the entire string

--- solutions/palindrome.cpp
+++ solutions/palindrome.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int main(){
 string ch;
- int n,m;
+ int n,m, flag =0;
   cout<<"enter the string:\n";
   cin>>ch;
   n=ch.length()-1; m=0;
   while(m!=n)                               // untile both the ends are not the same
-  {  if(ch[n]==ch[m])                       // check if the char from start index is equal to the char for equivallent index from the back,
-       { cout<<"pelindrom:\n";
+  {  if(ch[n]!=ch[m])                       // check if the char from start index is equal to the char for equivallent index from the back,
+       { flag =1 ; 
+        cout<<"\nNot palindrome";       //if any mismatch the loop is terminated
          break; 
        }
        m++;n--;                             // increasing the left index and decreasing the right by one.
   }
+ if (flag ==0 )
+  cout<<"\nPalindrome" ;
 return 0;
 }
